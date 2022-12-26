bbc9527aaf9f41375f84d8895d9337188b3b1924
nmtMike <minhtai96mike@gmail.com>
2022-12-26 23:04:53 +0700

    add selected function

--- warehouse_app/xuat_hang/views.py
+++ warehouse_app/xuat_hang/views.py
@@ -1,23 +1,24 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 import pandas as pd
 from warehouse_app.models import engine
 
 xuat_hang = Blueprint('xuat_hang', __name__)
 
-@xuat_hang.route('/soan_hang')
+@xuat_hang.route('/soan_hang', methods=['GET', 'POST'])
 def soan_hang():
     query = """ 
-    SELECT ma_san_pham, so_lo_ma_lo, ma_kho_tinh_trang, SUM(so_luong_xuat) AS so_luong
-        FROM misa
-        GROUP BY ma_san_pham, so_lo_ma_lo, ma_kho_tinh_trang
-        HAVING SUM(so_luong_xuat) > 0
-        ORDER BY ma_kho_tinh_trang
+    SELECT *
+        FROM don_hang_xuat
     """
-    df = pd.read_sql_query(query, engine)  
-    df['so_luong'] = df['so_luong'].astype(int)
-
+    df = pd.read_sql_query(query, engine)
     rows = df.values.tolist()
     column_names = df.columns.values
-    return render_template('danh_sach_soan_hang.html', rows=rows, column_names=column_names)
+    id_list = df['id'].tolist()
+
+    if request.method == 'POST':
+        id_checked = request.form.getlist('id_don_hang')
+        return f'imported, {id_checked}'
+
+    return render_template('danh_sach_soan_hang.html', len=len(rows), rows=rows, column_names=column_names, id_list=id_list)
 
 
